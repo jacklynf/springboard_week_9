24ff8c5ce5a04e71166d8d87a67f03c7e87ba949
jackie_aws <jackie@aws_workspace>
2023-03-25 23:45:25 -0700

    exercise 1 complete

--- src/game_lib/Game.cpp
+++ src/game_lib/Game.cpp
@@ -215,25 +215,34 @@ void Game::_handle_player_turn (
            << " is not getting out of the penalty box"
            << endl;
       // preserve existing bug behavior
-      if (!cur_player.response_is_correct()) {
-        ostr << "Question was incorrectly answered" << endl;
-        ostr << cur_player.get_name()
-             << " was sent to the penalty box"
-             << endl;
-      }
-      _broadcast(ostr.str());
+      //if (!cur_player.response_is_correct()){
+        //ostr << "Question was incorrectly answered." << endl;
+        //ostr << cur_player.get_name()
+            // << " was sent to the penalty box"
+             //<< endl;
+      //}
+      //_broadcast(ostr.str());
       return;
     }
     else
     {
       // preserve existing bug behavior
-      //cur_player.set_is_in_penalty_box(false);
-
-      ostringstream ostr(std::ostringstream::out);
-      ostr << cur_player.get_name()
-           << " is getting out of the penalty box"
-           << endl;
-      _broadcast(ostr.str());
+      if (cur_player.response_is_correct()){
+        cur_player.set_is_in_penalty_box(false);
+        ostringstream ostr(std::ostringstream::out);
+        ostr << cur_player.get_name()
+             << " is getting out of the penalty box"
+             << endl;
+        _broadcast(ostr.str());
+      }
+      else{
+        ostringstream ostr(std::ostringstream::out);
+        ostr << "Question was incorrectly answered.\n"
+             << cur_player.get_name()
+             << " is not getting out of the penalty box"
+             << endl;
+        _broadcast(ostr.str());
+      }
     }
   }
 
--- test/game_lib/test__Game.cc
+++ test/game_lib/test__Game.cc
@@ -150,8 +150,8 @@ SCENARIO ("player actions are handled correctly")
               REQUIRE(player_2.get_was_asked_question() == true);
             }
 
-            THEN ("the player does not get out of the penalty box") {
-              REQUIRE(player_2.get_is_in_penalty_box() == true);
+            THEN ("the player gets out of the penalty box") {
+              REQUIRE(player_2.get_is_in_penalty_box() == false);
             }
 
             THEN ("the player gets one gold coin") {
@@ -172,8 +172,8 @@ SCENARIO ("player actions are handled correctly")
               REQUIRE(player_2.get_place_idx() == 3);
             }
 
-            THEN ("the player is asked a question") {
-              REQUIRE(player_2.get_was_asked_question() == true);
+            THEN ("the player is not asked a question") {
+              REQUIRE(player_2.get_was_asked_question() == false);
             }
 
             THEN ("the player does not get out of the penalty box") {
@@ -229,8 +229,8 @@ SCENARIO ("player actions are handled correctly")
               REQUIRE(player_2.get_place_idx() == 3);
             }
 
-            THEN ("the player is asked a question") {
-              REQUIRE(player_2.get_was_asked_question() == true);
+            THEN ("the player is not asked a question") {
+              REQUIRE(player_2.get_was_asked_question() == false);
             }
 
             THEN ("the player does not get out of the penalty box") {
